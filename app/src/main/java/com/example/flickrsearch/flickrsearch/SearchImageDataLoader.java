8033a60f66af20cb5e522130c698341e885c25d7
Meghana Mokashi <meghana_mokashi@rediffmail.com>
2018-08-26 11:18:41 +0530

    Code refactoring

--- app/src/main/java/com/example/flickrsearch/flickrsearch/SearchImageDataLoader.java
+++ app/src/main/java/com/example/flickrsearch/flickrsearch/SearchImageDataLoader.java
@@ -3,10 +3,10 @@ package com.example.flickrsearch.flickrsearch;
  * Created by Meghana Mokashi
  * Copyright (c) 2018. All rights reserved.
  */
+
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
 import android.util.Log;
 
 import com.android.volley.Request;
